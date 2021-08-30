7c75a6df7a8c9d74e96df7ce9696ace7cce10573
Yong-Wei-Han <yongwh-wp18@student.tarc.edu.my>
2021-08-30 22:15:54 +0800

    ywh

---
+++ Cloud Assignment/Gallery.aspx.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cloud_Assignment
+{
+    public partial class Gallery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if (Session["Bar"] == "C")
+            {
+               
+                if (e.CommandName == "AddToCart")
+                {
+                    if (Session["Value"] != null && Session["Value"] != "0")
+                    {
+                        SqlConnection con;
+                        string strcon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                        con = new SqlConnection(strcon);
+                        string addedDraw = e.CommandArgument.ToString();
+                        Boolean duplicate = false;
+                        //checking value
+                        con.Open();
+                        string strSelectChecking = "Select * from CartGallery Where DrawID=@DrawID and CustomerID = @CustomerID";
+                        SqlCommand cmdSelect = new SqlCommand(strSelectChecking, con);
+
+                        cmdSelect.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                        cmdSelect.Parameters.AddWithValue("@DrawID", addedDraw);
+
+                        SqlDataReader dtr = cmdSelect.ExecuteReader();
+
+                        if (dtr.HasRows)
+                        {
+                            while (dtr.Read())
+                            {
+                                if (addedDraw.Equals(dtr["DrawID"]) && Session["Value"].Equals(dtr["CustomerID"]))
+                                {
+                                    duplicate = true;
+                                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Already added! " + "');", true);
+                                }
+                            }
+                        }
+
+                        con.Close();
+
+
+                        //insert value
+                        if (!duplicate)
+                        {
+                            con.Open();
+                            string strSelect1 = "Select Price from Gallery Where DrawID=@DrawID";
+                            SqlCommand cmdSelect1 = new SqlCommand(strSelect1, con);
+
+                            cmdSelect1.Parameters.AddWithValue("@DrawID", addedDraw);
+                            System.Decimal totalPrice = (System.Decimal)cmdSelect1.ExecuteScalar();
+
+                            con.Close();
+
+                            con.Open();
+
+                            string strInsert = "Insert into CartGallery (CustomerID, DrawID,TotalPrice) Values (@CustomerID, @DrawID,@TotalPrice)";
+
+                            SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+                            cmdInsert.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                            cmdInsert.Parameters.AddWithValue("@DrawID", addedDraw);
+                            cmdInsert.Parameters.AddWithValue("@TotalPrice", totalPrice);
+                            //cmdInsert.Parameters.AddWithValue("@Name", "Name");
+                            //cmdInsert.Parameters.AddWithValue("@Price", "Price");
+                            //cmdInsert.Parameters.AddWithValue("@Image", "Image");
+
+                            int numRowAffected1 = cmdInsert.ExecuteNonQuery();
+                            if (numRowAffected1 > 0)
+                            {
+                                // return insert success
+                                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Added successfully! " + "');", true);
+                            }
+                            else
+                            {
+                                // return insert failed
+                                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "update failed! " + "');", true);
+                            }
+                            con.Close();
+                        }
+                    }
+                    else
+                    {
+                        // not allow to add, please sign in first
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "log in first! " + "');", true);
+                    }
+                }
+                else
+                {
+                    if (Session["Value"] != null && Session["Value"] != "0")
+                    {
+                        SqlConnection con;
+                        string strcon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                        con = new SqlConnection(strcon);
+                        string addedDraw = e.CommandArgument.ToString();
+                        Boolean duplicate = false;
+                        //checking value
+                        con.Open();
+                        string strSelectChecking = "Select * from WishlistGallery Where DrawID=@DrawID and CustomerID = @CustomerID";
+                        SqlCommand cmdSelect = new SqlCommand(strSelectChecking, con);
+
+                        cmdSelect.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                        cmdSelect.Parameters.AddWithValue("@DrawID", addedDraw);
+
+                        SqlDataReader dtr = cmdSelect.ExecuteReader();
+
+                        if (dtr.HasRows)
+                        {
+                            while (dtr.Read())
+                            {
+                                if (addedDraw.Equals(dtr["DrawID"]) && Session["Value"].Equals(dtr["CustomerID"]))
+                                {
+                                    duplicate = true;
+                                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Already added! " + "');", true);
+                                }
+                            }
+                        }
+
+                        con.Close();
+
+                        //insert value
+                        if (!duplicate)
+                        {
+                            con.Open();
+
+                            string strInsert = "Insert into WishlistGallery (CustomerID, DrawID, Date) Values (@CustomerID, @DrawID,@Date)";
+
+                            SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+                            cmdInsert.Parameters.AddWithValue("@CustomerID", Session["Value"]);
+                            cmdInsert.Parameters.AddWithValue("@DrawID", addedDraw);
+                            cmdInsert.Parameters.AddWithValue("@Date", DateTime.Now);
+
+                            int numRowAffected = cmdInsert.ExecuteNonQuery();
+                            if (numRowAffected > 0)
+                            {
+                                // return insert success
+                                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Added successfully! " + "');", true);
+                            }
+                            else
+                            {
+                                // return insert failed
+                                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "update failed! " + "');", true);
+                            }
+                            con.Close();
+                        }
+                    }
+                    else
+                    {
+                        // not allow to add, please sign in first
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "log in first! " + "');", true);
+                    }
+                }
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Please log in as a customer first before making any purchase! " + "');", true);
+            }
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = "SELECT * FROM [Gallery] Where [Total] > 0 and Category='T-shirt'";
+        }
+
+        protected void ShowAll_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = "SELECT * FROM [Gallery] Where [Total] > 0";
+        }
+
+        protected void SculptureFilter_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = "SELECT * FROM [Gallery] Where [Total] > 0 and Category='Collar'";
+        }
+
+        protected void Painting_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = "SELECT * FROM [Gallery] Where [Total] > 0 and Category='Painting'";
+        }
+    }
+}
---
+++ Cloud Assignment/SellerUploadShirt.aspx
@@ -0,0 +1,161 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SyaSyaDesign.Master" AutoEventWireup="true" CodeBehind="SellerUploadShirt.aspx.cs" Inherits="Cloud_Assignment.SellerUploadShirt" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <h2 style="text-align:center">Art Selling</h2><hr />
+    <div style="width:75%; margin:0 auto; min-height:410px">
+            <style type="text/css">
+
+        .auto-style2 {
+            height: 29px;
+            width: 118px;
+        }
+        .auto-style3 {
+            width: 279px;
+        }
+        .auto-style4 {
+            height: 29px;
+            width: 279px;
+        }
+        .auto-style5 {
+            height: 24px;
+            width: 118px;
+            font-weight:bold;
+        }
+        .auto-style6 {
+            width: 279px;
+            height: 24px;
+        }
+        .auto-style7 {
+            width: 118px;
+            margin-left:10px;
+
+        }
+        .auto-style8 {
+            border-collapse: collapse;            
+            height: auto;
+            width: 873px;
+            background-color: lightgray;
+            margin-bottom : 1%;
+            height: 300px;
+        }
+        .button{
+            border-color: #4D94FF; 
+            background-color: white; 
+            color: #284E98;"
+        }
+    </style>
+
+     <% if (Session["Value"] == "0" || Session["Value"] == null || Session["Bar"] == "C")
+          { %>
+             <p>Please <a href="LogIn.aspx" > log in</a> as artist before accessing this page.</p>
+             <p>OR<a href="Register.aspx" > register</a> as artist before accessing this page.</p>
+      <%}
+          else{ %>
+            <h1 style="text-align:center;">Post Art</h1><hr />   
+            
+            <h1>Welcome to Alzenda Artwork Sales Gallery , select upload to post the art online !!!</h1>
+            <p>
+                Wanted to sell your masterpiece online? Want to let your art to be recognized by others? Post your art now online at Alzenda Artwork Sales Gallery !
+            </p>
+            <br />
+                
+    <table class ="auto-style8">
+
+        <tr>
+            <td class="auto-style5">Art Details</td>
+            <td class="auto-style6">
+                </td>
+        </tr>   
+
+        <tr>
+            <td class="auto-style7">Art name : </td>
+            <td class="auto-style3">
+                <asp:TextBox ID="ArtName" runat="server" Width="267px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="*This field is required." SetFocusOnError="True" ControlToValidate="ArtName" ForeColor="Red" ValidationGroup="postArt"></asp:RequiredFieldValidator>
+            </td>
+        </tr>   
+
+        <tr>
+            <td class="auto-style7">
+                Art Description :
+            </td>
+            <td class="auto-style3">
+                <asp:TextBox runat="server" ID="ArtDescription" TextMode="Multiline" Columns="20" Name="S1" Rows="2" Height="95px" Width="591px" style="resize:none;"></asp:TextBox>
+                <br />
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="*This field is required." SetFocusOnError="True" ControlToValidate="ArtDescription" ForeColor="Red" ValidationGroup="postArt"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="auto-style7">
+                Price :
+            </td>
+            <td class="auto-style3">
+                <asp:TextBox ID="Price" runat="server" Width="265px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="*This field id required." ControlToValidate="Price" SetFocusOnError="True" ForeColor="Red" ValidationGroup="postArt"></asp:RequiredFieldValidator>
+                <br />
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="Please Enter Your Correct Product Price" ControlToValidate="Price" ValidationExpression="^\d{0,8}(\.\d{1,2})?$" SetFocusOnError="True" ForeColor="Red" ValidationGroup="postArt"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td>Category :</td>
+            <td>
+                <asp:DropDownList ID="CategoryList" runat="server">
+                    <asp:ListItem>T-shirt</asp:ListItem>
+                    <asp:ListItem>Collar</asp:ListItem>
+                    <asp:ListItem>dummy</asp:ListItem>
+                </asp:DropDownList></td>
+        </tr>
+        <tr>
+            <td class="auto-style2">
+                Total Art :
+            </td>
+            <td class="auto-style4">
+                <asp:TextBox ID="TotalArt" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="auto-style7">Art Drawing :</td>
+            <td class="auto-style3">
+                <asp:FileUpload ID="Picture" runat="server" accept="image/*" />
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ErrorMessage="*This field id required." ControlToValidate="Picture" SetFocusOnError="True" ForeColor="Red" ValidationGroup="postArt"></asp:RequiredFieldValidator>
+                </td>
+        </tr>
+
+        <tr>
+            <td class="auto-style7">&nbsp;</td>
+            <td class="auto-style3">
+                &nbsp;</td>
+        </tr>
+
+        <tr>
+            <td class="auto-style7" style="text-align:center">
+         <asp:Button ID="Cancel" class="button" runat="server" Text="Cancel" Width="140px" OnClick="Cancel_Click"  />
+            </td>
+            <td class="auto-style3">
+        <asp:Button ID="Submit" class="button" runat="server" Text="Submit" OnClick="Submit_Click" Width="139px" ValidationGroup="postArt"/>
+                
+            </td>        
+        </tr>       
+        <tr>
+            <td>
+            <br/>
+            </td>
+            <td>
+            <br/>
+            </td>
+        </tr>
+
+        </table>
+     
+        
+          <%} %>
+    <br />
+            &nbsp;
+    <div >
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+    </div>
+</asp:Content>
